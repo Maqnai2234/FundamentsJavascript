a52cd6663413f11d2b246e8847f8827d63228994
Alexei <alexeim763@gmail.com>
2017-09-14 00:14:04 -0500

    objects Javascript

--- objects1.js
+++ objects1.js
@@ -1,17 +1,31 @@
 const p1 = {
   x: 0,
-  y: 4
+  y: 4,
+  moverX: function(x){
+    this.x += x
+  },
+  moverY: function(y){
+    this.y += y
+  }
 }
 
 const p2 = {
   x: 3,
-  y: 0
+  y: 0,
+  moverX: function(x){
+    this.x += x
+  },
+  moverY: function(y){
+    this.y += y
+  }
 }
 
 function distance(p1,p2){
   const x = p1.x - p2.x
   const y = p1.y - p2.y
-  return Math.sqrt(x * x + y * y)
+  return parseFloat(Math.sqrt(x * x + y * y).toFixed(2))
 }
 
-console.log(`La distancia entre ${distance(p1,{x: 20, y: -7})}`)
+p1.moverX(5)
+
+console.log(`La distancia entre ${distance(p1,p2)}`)
